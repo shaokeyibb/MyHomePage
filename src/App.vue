1d60b66f8593676d962e5a836498f02750ab1e94
HikariLan <shaokeyibb@126.com>
2021-09-25 19:18:55 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app style="
+  background-image: url('https://api.fantasyzone.cc/tu')
+">
+    <v-main style="
+    position: absolute;
+    top: 0; bottom: 0;
+    left: 0; right: 0;
+    line-height: 2;
+    margin: auto;
+    border-radius: 5px;
+    background: rgba(255, 255, 255, .6);
+    box-shadow: 3px 3px 6px 3px rgba(0, 0, 0, .3);
+    overflow: hidden;">
+      <router-view/>
+    </v-main>
+    <v-footer
+        color="green"
+        style="opacity: 0.5"
+        absolute>
+      <v-col
+          class="text-center"
+          style="line-height: normal"
+          cols="12"
+      >
+        Copyright © by HikariLan All Rights Reserved. <a href="https://beian.miit.gov.cn/">陕ICP备2020016655号-1</a>
+        <br>
+        Powered by Vue.js && Vuetify
+        <br>
+        The random cover API by <a href="https://api.fantasyzone.cc/">FantasyZone Public API</a>
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-avatar size="200">
+          <v-img :src="'http://q1.qlogo.cn/g?b=qq&nk='+social[2].number+'&s=640'"/>
+        </v-avatar>
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          这里是 HikariLan 贺兰星辰😀
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          一个普通的学生，业余开发者，业余-业余运维，喜欢折腾新事物
+        </p>
+
+        <v-row justify="center">
+          <div v-for="(it,i) in social" :key="i" style="padding: 5px">
+            <v-btn
+                :key="i"
+                link="link"
+                :href="it.link"
+                outlined
+                rounded
+                x-large
+            >{{ it.name }}
+            </v-btn>
+          </div>
+        </v-row>
+      </v-col>
+
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+        social: [{
+          name: "主页",
+          link: "#"
+        }, {
+          name:"博客",
+          link: "https://my.minecraft.kim"
+        },
+          {
+            name: "QQ",
+            link: "tencent://message/?uin=2191023046",
+            number: 2191023046
+          }, {
+            name: "知乎",
+            link: "https://www.zhihu.com/people/shaokeyibb"
+          }, {
+            name: "B站",
+            link: "https://space.bilibili.com/29367276"
+          }, {
+            name: "GitHub",
+            link: "https://github.com/shaokeyibb"
+          }],
+      }
+  ),
+}
+</script>
